f094ea14ceb21348767875ed9256d4247e0d175a
Nassim BENDAOUD <bassimbassim06@gmail.com>
2025-12-13 10:49:21 +0100

    Add files via upload

--- dz-kitab-frontend/src/components/header.jsx
+++ dz-kitab-frontend/src/components/header.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import { FiMessageSquare } from "react-icons/fi";
 import { CiSearch } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { IoIosNotificationsOutline } from "react-icons/io";
 const Header = () => {
-    return (
-        <header>
-            <div className="guest-header">
-                <div className="website-title">
-                    <img className='logo' src='./dz-kitablogo.png'></img>
-                    <a className="a"><h3><span>DZ</span>-KITAB</h3></a>
-                </div>
-                <div className="links">
-                    <a>Home</a>
-                    <a>About us</a>
-                    <a>Contact us</a>
-                </div>
-                <div className="buttons">
-                    <button className='login-button'>Login</button>
-                    <button className='register-button'>Register</button>
-                </div>
-            </div>
+  return (
+    <header>
+      <div className="guest-header">
+        <div className="website-title">
+          <img className="logo" src="./dz-kitablogo.png"></img>
+          <a className="a">
+            <h3>
+              <span>DZ</span>-KITAB
+            </h3>
+          </a>
+        </div>
+        <div className="links">
+          <a>Home</a>
+          <a>About us</a>
+          <a>Contact us</a>
+        </div>
+        <div className="buttons">
+          <Link to="/login">
+            <button className="login-button">Login</button>
+          </Link>
+          <button className="register-button">Register</button>
+        </div>
+      </div>
 
-            {/* <div className="user-header">
+      {/* <div className="user-header">
                 <div className="website-title">
                     <img className='logo' src='./dz-kitablogo.png'></img>
                     <a className="a"><h3><span>DZ</span>-KITAB</h3></a>
@@ -47,8 +54,8 @@ const Header = () => {
 
                 </div>
             </div> */}
-        </header>
-    )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
