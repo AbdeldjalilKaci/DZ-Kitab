6311bf35aa60a6e5b0700c9d2436675c7358be7f
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-14 12:53:41 +0000

    fix some changes

--- dz-kitab-frontend/src/components/landingpage-sections/category.jsx
+++ dz-kitab-frontend/src/components/landingpage-sections/category.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { CategoryCard } from '../category-card'
 
 export const Category = () => {
+    const categories = [
+        'Math Books',
+        'AI Books',
+        'Science Books',
+        'Medical Books',
+        'Business Books',
+        'Arabic Books'
+    ]
+
     return (
-        <section className='category-section'>
-            <h3>Browser by <span>Book Types</span></h3>
+        <section className="category-section">
+            <h3>Browse by <span>Book Types</span></h3>
+
             <div className="categorys">
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./math2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
-                <CategoryCard title={'Math Books'} image={'./ai2.jpg'}  />
+                {categories.map((category, index) => (
+                        <CategoryCard
+                            key={index}
+                            title={category}
+                            image="./ai2.jpg"
+                        />
+                ))}
             </div>
         </section>
     )
