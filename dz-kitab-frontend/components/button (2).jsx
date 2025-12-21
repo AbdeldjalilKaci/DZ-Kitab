18cffc432bc605c2471bb994646efe4f9d9b07c7
Nassim BENDAOUD <n_bendaoud@estin.dz>
2025-12-21 13:20:38 +0100

    Delete dz-kitab-frontend/components directory

--- dz-kitab-frontend/components/button (2).jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Button = ({text,color,width,height,bordercolor,borderadius,backgroundcolor}) => {
-    return (
-        <button className='.button' style={{width:width,height:height,color:color,borderRadius:borderadius,backgroundColor:backgroundcolor,borderColor:bordercolor}}>
-            {text}
-        </button>
-    )
-}
