04d443b607c6e12af772e59dbeb602f409b0a2fc
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-22 19:25:09 +0000

    add messages page and fix announce page

--- dz-kitab-frontend/src/components/landingpage-sections/hero.jsx
+++ dz-kitab-frontend/src/components/landingpage-sections/hero.jsx
@@ -11,13 +11,14 @@ export const Hero = () => {
             }}
             className="relative hero min-h-[500px] w-full"
         >
-            {/* Black overlay */}
             <div className="absolute inset-0 bg-black/40"></div>
             <div className="hero-info z-50 ">
-                <h3 className=' text-white' >
+                <h3 className='text-white text-3xl md:text-5xl font-bold mb-4 max-w-2xl'>
                     DZ-Kitab – La Révolution du Livre en Algérie
                 </h3>
-                <p className='text-gray-200 w-120 '> La plateforme où les étudiants achètent et vendent leurs manuels. Économisez jusqu'à 70% sur vos livres de cours.</p>
+                <p className='text-gray-200 text-lg md:text-xl w-full max-w-xl mb-8 leading-relaxed'>
+                    La plateforme où les étudiants achètent et vendent leurs manuels. Économisez jusqu'à 70% sur vos livres de cours.
+                </p>
                 {/* <div className="search">
                     <input type='search' placeholder='Search...' className='search-input'></input>
                     <button className="search-button">
